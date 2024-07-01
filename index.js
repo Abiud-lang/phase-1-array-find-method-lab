2812010831266f2f1a68fce534fe2055959c5715
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-01 22:28:17 +0300

    find methods

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+const records = [
+  { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    ]
+
+function superbowlWin(records) {
+  const win = records.find(record => record.result === "W");
+  return win ? win.year : undefined;
+}
